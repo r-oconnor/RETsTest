bb4add9efbb0935015d696feb822e60e1b7918c5
Terrence Young <tyoung@solstice-consulting.com>
2016-12-14 12:16:32 -0600

    Add project files.

---
+++ RETsTest/ConnectToEELE.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using librets;
+using System.Collections;
+namespace RETsTest
+{
+    public class ConnectToEELE
+    {
+        static void Main(string[] args)
+        {
+            bool isLoggedIn = false;
+           
+            using (RetsSession retssession = new RetsSession("http://www.eele-rets.com:6160/rets/login"))
+            {
+                
+                retssession.UseHttpGet(true);
+                //retssession.
+                isLoggedIn = retssession.Login("corcoran", "JDXzmkGa");
+                              
+                if (isLoggedIn)
+                {
+                  
+                    var urls = retssession.GetCapabilityUrls();
+                    Console.WriteLine(urls.GetLoginUrl());
+
+                    RetsMetadata metadata = retssession.GetMetadata();
+                    IEnumerable resources = metadata.GetAllResources();
+                 
+                    using (SearchRequest searchRequest = retssession.CreateSearchRequest(
+                                                    "Property",
+                                                    "CLISTINGS",
+                                                    "(Modified=2016-12-12T00:00:00+)"))
+                    {
+                        searchRequest.SetQueryType(SearchRequest.QueryType.DMQL2);
+                        searchRequest.SetStandardNames(false);
+                        searchRequest.SetOffset(SearchRequest.OFFSET_NONE);
+                        SearchResultSet results = retssession.Search(searchRequest);
+                        Console.WriteLine("Record count: " + results.GetCount());
+                        Console.WriteLine();
+                        IEnumerable columns = results.GetColumns();
+                        while (results.HasNext())
+                        {
+                            foreach (string column in columns)
+                            {
+                                Console.WriteLine(column + ": " + results.GetString(column));
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+
+               
+
+            }
+        }
+
+        static void dumpAllClasses(RetsMetadata metadata,
+                                   MetadataResource resource)
+        {
+            string resourceName = resource.GetResourceID();
+            IEnumerable classes = metadata.GetAllClasses(resourceName);
+            foreach (MetadataClass aClass in classes)
+            {
+                Console.WriteLine("Resource name: " + resourceName + " [" +
+                    resource.GetStandardName() + "]");
+                Console.WriteLine("Class name: " + aClass.GetClassName() +
+                    " [" + aClass.GetStandardName() + "]");
+                dumpAllTables(metadata, aClass);
+                Console.WriteLine();
+            }
+        }
+        static void dumpAllTables(RetsMetadata metadata, MetadataClass aClass)
+        {
+            IEnumerable tables = metadata.GetAllTables(aClass);
+            foreach (MetadataTable table in tables)
+            {
+                Console.WriteLine("Table name: " + table.GetSystemName() + " [" +
+                    table.GetStandardName() + "]");
+                Console.WriteLine("\tTable datatype: " + table.GetDataType());
+                Console.WriteLine("\tUnique: " + table.IsUnique());
+                Console.WriteLine("\tMax Length: " + table.GetMaximumLength());
+            }
+        }
+    }
+}
---
+++ RETsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RETsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RETsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4fd333a-c8b9-4265-8003-45acb3c2a316")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
